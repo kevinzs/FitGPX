1f0a22b55ebfe190f2a0bdddf9dcd393182d722d
MOCHOLI CALVO <carmocca@LX-01.dsic.cloud>
2016-05-18 16:14:15 +0200

    Añadida fecha en diario de actividad

--- src/controller/ActivityDiaryViewController.java
+++ src/controller/ActivityDiaryViewController.java
@@ -3,6 +3,8 @@ package controller;
 
 import java.net.URL;
 import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.ResourceBundle;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
@@ -12,6 +14,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.XYChart;
+import javafx.scene.chart.XYChart.Data;
 import javafx.scene.control.ComboBox;
 import utilities.TracksList;
 
@@ -58,18 +61,34 @@ public class ActivityDiaryViewController implements Initializable {
             default:
                 break;
         }
+        LocalDateTime todai = LocalDateTime.now();
+        int today = date.getDayOfYear();
         seriesTiempo = new XYChart.Series();
         seriesDistancia = new XYChart.Series();
         seriesTiempo.setName("Tiempo");
         seriesDistancia.setName("Distancia");
         for(int i=0; i<tracksList.getTracks().size(); i++){
             if(tracksList.getTracks().get(i).getStartTime().compareTo(date) >= 0){
-                seriesTiempo.getData().add(new XYChart.Data<>(""+tracksList.getTracks().get(i).getStartTime().getDayOfYear(), 
+                int days = today - tracksList.getTracks().get(i).getStartTime().getDayOfYear();
+                seriesTiempo.getData().add(new XYChart.Data<>(todai.minusDays(days).toString().substring(0, 10), 
                         tracksList.getTrackData(i).getTotalDuration().toMinutes()));
-                seriesDistancia.getData().add(new XYChart.Data<>(""+tracksList.getTracks().get(i).getStartTime().getDayOfYear(), 
+                seriesDistancia.getData().add(new XYChart.Data<>(todai.minusDays(days).toString().substring(0, 10), 
                         tracksList.getTrackData(i).getTotalDistance()));
             }
         }
+        
+        /*http://stackoverflow.com/questions/29288669/javafx-how-to-sort-values-in-a-barchart
+        Collections.sort(seriesTiempo.getData(), new Comparator<XYChart.Data>() {
+            @Override
+            public int compare(Data o1, Data o2) {
+                String xValue1 = (String) o1.getXValue();
+                int xValueYear, xValueMonth, xValueDay;
+                xValueYear = 
+                String xValue2 = (String) o2.getXValue();
+                return new BigDecimal(xValue1.toString()).compareTo(new BigDecimal(xValue2.toString()));
+            }
+        });*/
+        
         barChart.getData().clear();
         barChart.getData().addAll(seriesTiempo);
         barChart2.getData().clear();
